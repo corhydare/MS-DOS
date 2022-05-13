37cc763a3a3569c3a033114ca9a3af045913f63c
Unknown <danila.vpopov@gmail.com>
2022-05-13 15:41:34 -0700

    chchchanges

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,28 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("msdos", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("msdos")) {
+        console.log("msdos database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("msdos", { keyPath: "id", autoIncrement: true });
+      console.log("msdos database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log("opening msdosDB");
+  const msdosDb = await openDB("msdos", 1);
+  const text = msdosDb.transaction("msdos", "readwrite");
+  const store = text.objectStore("msdos");
+  const request = store.put({ value: content });
+  const result = await request;
+  console.warn(result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {};
 
 initdb();
